e5b4620b9fcc3053ece97bc89fc198f61332e8cb
lvdovicb <lvdovicbenoit@gmail.com>
2018-02-20 14:22:39 +0100

    odjp

---
+++ default/src/dao/DaoPost.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace simplon\dao;
+use simplon\entities\posts;
+use simplon\dao\Connect;
+
+class Post {
+
+  public function getAll():array {
+    $tab = [];
+
+    try {
+      // $pdo = new \PDO('mysql:host=mysql;dbname=db;','root','root');
+      /*On utilise la méthode prepare() de notre connexion pour préparer
+      une requête SQL (elle n'est pas envoyée tant qu'on ne lui dit pas)
+      La méthode prepare attend en argument une string SQL
+      */
+      $query = Connect::getInstance()->prepare('SELECT * FROM posts');
+      //On dit à notre requête de s'exécuter, à ce moment là, le résultat
+      //de la requête est disponible dans la variable $query
+      $query->execute();
+      /*On itère sur les différentes lignes de résultats retournées par
+      notre requête en utilisant un $query->fetch qui renvoie une ligne
+      de résultat sous forme de tableau associatif tant qu'il y a des
+      résultat. On stock donc le retour de ce fetch dans une variable 
+      $row et on boucle dessus
+      */
+      while($row = $query->fetch()) {
+          /*
+          A chaque tour de boucle, on se sert de notre ligne de résultat
+          sous forme de tableau associatif pour créer une instance de 
+          Person en lui donnant en argument les différentes valeurs des
+          colonnes de la ligne de résultat.
+          Les index de $row correspondent aux noms de colonnes dans notre
+          SQL.
+          */
+          $post = new Post($row['title'], 
+                           $row['id']);
+          //On ajoute la person créée à notre tableau
+          $tab[] = $post;
+      }
+  }catch(\PDOException $e) {
+      echo $e;
+  }
+  //On return le tableau
+  return $tab;
+}
+/**
+* Méthode permettant de récupérer une Person en se basant sur
+* son Id
+* @return User|null renvoie soit la Person correspondante soit null
+* si pas de match
+*/
+public function getById(int $id) {
+  
+  try {
+      /**
+       * On prépare notre requête, mais cette fois ci, nous avons un
+       * argument à insérer dans la requête : l'id.
+       * La concaténation est absolument déconseillé dans les string
+       * SQL car ça ouvrirait notre code aux injections SQL qui sont
+       * un soucis très grave.
+       * A la place, on met un placeholder dans la requête auquel on
+       * donne un label précédé de :, par exemple :id
+       */
+      $query = Connect::getInstance()->prepare('SELECT * FROM posts WHERE id=:id');
+      /**
+       * Chaque placeholder d'une requête doit être bindée, soit par
+       * un bindValue, soit directement dans le execute via un 
+       * tableau associatif.
+       * Ici, on dit qu'on met la valeur de la variable $id, là où
+       * on a mis le :id dans la requête, et on indique que la 
+       * valeur en question doit être de type int
+       */
+      $query->bindValue(':id', $id, \PDO::PARAM_INT);
+      //On exécute la requête
+      $query->execute();
+      //Si le fetch nous renvoie quelque chose
+      if($row = $query->fetch()) {
+          //On crée une instance de Person
+          $post = new Post ($row['name'], 
+                            $row['id']);
+          //On return cette Person
+          return $post;
+      }
+  }catch(\PDOException $e) {
+      echo $e;
+  }
+  /**
+   * Si jamais on est pas passé dans le if ou autre, on renvoie null
+   * qui est une valeur inexistante. C'est quelque chose d'assez
+   * utilisé dans beaucoup de langages. 
+   */
+  return null;
+}
+/**
+* Méthode permettant de faire persister en base de données une 
+* instance de Person passée en argument.
+*/
+public function add(Post $post) {
+  
+  try {
+      //On prépare notre requête, avec les divers placeholders
+      $query = Connect::getInstance()->prepare('INSERT INTO posts (title) VALUES (:title)');
+      /**
+       * On bind les différentes values qu'on récupère de l'instance
+       * de Person qui nous est passée en argument, via ses
+       * accesseurs get*()
+       */
+      $query->bindValue(':title',$user->getName(),\PDO::PARAM_STR);
+
+      $query->execute();
+      /**
+       * On fait en sorte de récupérer le dernier id généré par SQL 
+       * afin de l'assigner à l'id de notre instance de Person
+       */
+      $post->setId(Connect::getInstance()->lastInsertId());
+      
+  }catch(\PDOException $e) {
+      echo $e;
+  }
+}
+/**
+* Une méthode pour mettre à jour les informations d'une Person 
+* déjà existante dans la base de donnée.
+* L'argument $pers doit être une instance de Person complète, avec
+* un id existant en base.
+*/
+public function update(Post $post) {
+  
+  try {
+      //toujours pareil, on prépare la requête
+      $query = Connect::getInstance()->prepare('UPDATE posts SET title =:title WHERE id = :id');
+      //on bind les value des placeholders
+      $query->bindValue(':title',$user->getName(),\PDO::PARAM_STR);
+      $query->bindValue(':id',$post->getId(), \PDO::PARAM_INT);
+
+      //on exécute la requête
+      $query->execute();
+      
+      
+  }catch(\PDOException $e) {
+      echo $e;
+  }
+}
+
+/**
+* La méthode delete supprimera une Person de la base de données en
+* se basant sur son id
+*/
+public function delete(int $id) {
+  
+  try {
+      //On prépare...
+      $query = Connect::getInstance()->prepare('DELETE FROM posts WHERE id = :id');
+      //on bind...
+      $query->bindValue(':id',$id,\PDO::PARAM_INT);
+
+      //on exécute
+      $query->execute();
+      
+      
+  }catch(\PDOException $e) {
+      echo $e;
+  }
+}
+};
---
+++ default/src/routes.php
@@ -0,0 +1,85 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use simplon\entities\User;
+use simplon\dao\DaoUser;
+
+// Routes
+
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    //On instancie le dao
+    $dao = new DaoUser();
+    //On récupère les Persons via la méthode getAll
+    $users = $dao->getAll();
+    //On passe les persons à la vue index.twig
+    return $this->view->render($response, 'index.twig', [
+        'users' => $users
+    ]);
+})->setName('index');
+
+
+
+$app->get('/adduser', function (Request $request, Response $response, array $args) {
+    return $this->view->render($response, 'adduser.twig');
+})->setName('adduser');
+
+$app->post('/adduser', function (Request $request, Response $response, array $args) {
+    //On récupère les données du formulaire
+    $form = $request->getParsedBody();
+    //On crée une Person à partir de ces données
+    $newUser = new User($form['name'], $form['email'], $form['password']);
+    //On instancie le DAO
+    $dao = new DaoUser();
+    //On utilise la méthode add du DAO en lui donnant la Person qu'on vient de créer
+    $dao->add($newUser);
+    //On affiche la même vue que la route en get
+    return $this->view->render($response, 'adduser.twig', [
+        'newId' => $newUser->getId()
+    ]);
+})->setName('adduser');
+
+
+
+$app->get('/updateuser/{id}', function (Request $request, Response $response, array $args) {
+    //On instancie le DAO
+    $dao = new DaoUser;
+    //On récupère la Person à partir de l'id
+    $user = $dao->getById($args['id']);
+    // On affiche la vue du formulaire d'update d'une peronne
+    return $this->view->render($response, 'updateuser.twig', [
+        'user' => $user
+    ]);
+    
+})->setName('updateuser');
+
+$app->post('/updateuser/{id}', function (Request $request, Response $response, array $args) {
+    //On instancie le DAO
+    $dao = new DaoUser;
+    //On récupère les données du formulaire
+    $postData = $request->getParsedBody();
+    //On récupère la Person à partir de l'id
+    $user = $dao->getById($args['id']);
+    //On met à jour son nom, sa date de naissance et son genre
+    $user->setName($postData['name']);
+    $user->setEmail($postData['email']);
+    $user->setPassword($postData['password']);
+    //On update la personne
+    $dao->update($user);
+    //On récupère l'URL da la route index (page d'accueil)
+    $redirectUrl = $this->router->pathFor('index');
+    //On redirige l'utilisateur sur la page d'accueil
+    return $response->withRedirect($redirectUrl);
+})->setName('updateuser');
+
+$app->get('/deleteuser/{id}', function (Request $request, Response $response, array $args) {
+    //On instancie le DAO
+    $dao = new DaoUser;
+    //On delete la personne
+    $dao->delete($args['id']);
+    //On récupère l'URL da la route index (page d'accueil)
+    $redirectUrl = $this->router->pathFor('index');
+    //On redirige l'utilisateur sur la page d'accueil
+    return $response->withRedirect($redirectUrl);
+})->setName('deleteuser');
