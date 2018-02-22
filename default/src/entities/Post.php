0cb41b531ce4cecc306be681ee15f347e75deb53
lvdovicb <lvdovicbenoit@gmail.com>
2018-02-22 17:27:08 +0100

    essai show posts

---
+++ default/src/entities/Post.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace simplon\entities;
+
+class Post {
+    private $id;
+    private $title;
+    private $content;
+    
+
+    public function __construct(string $title,
+                                string $content,
+                                int $id=null) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->content =  $content;
+      
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+};
+
+    
